59daf447723ed13dc0ee045f942b09be53f99cec
hibana <421786477@qq.com>
2022-04-10 15:52:27 +0800

    pm2

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import type { UserConfig } from 'ssr-types'
 
 const userConfig: UserConfig = {
-	whiteList: ['axios', '@mx-space/api-client', '@milkdown/core', '@milkdown/vue'],
+	whiteList: ['axios', '@mx-space/api-client'],
 	css: () => {
 		return {
 			loaderOptions: {
@@ -16,7 +16,7 @@ const userConfig: UserConfig = {
 	// babelOptions: {
 	// 	plugins: ['@babel/plugin-proposal-optional-chaining'] // 通常使用该配置新增 plugin
 	// },
-	babelExtraModule: [/@mx-space\/api-client/, /milkdown/],
+	babelExtraModule: [/@mx-space\/api-client/],
 	proxy: {
 		'https://server.hibana.xyz/api/v2': {
 			target: 'https://server.hibana.xyz/api/v2',
